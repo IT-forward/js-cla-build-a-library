f8660b0e0097463825c057b79b4743e0ca0bc932
bekji <turayev07@gmail.com>
2020-12-29 11:50:05 +0500

    first version

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 import { Media, Book, Movie, CD } from "./resources.js";
 
-// TODO
+const bookObj = new Book("James Clear", "Atomic Habits", 320);
+
+console.log(bookObj.author);
+console.log(bookObj.title);
+console.log(bookObj.pages);
+console.log(bookObj.isCheckedOut);
+console.log(bookObj.ratings);
+
+bookObj.addRating(50);
+bookObj.addRating(60);
+bookObj.addRating(70);
+
+console.log(bookObj.getAverageRating());
+
+console.log(bookObj.ratings);
+
+bookObj.toggleCheckOutStatus();
+console.log(bookObj.isCheckedOut);
