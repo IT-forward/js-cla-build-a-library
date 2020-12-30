074f2186680d995c11721cb146b782822bdb2a8b
Husan-Eshonqulov <husan.eshonqulov.00@gmail.com>
2020-12-30 11:33:46 +0300

    first version of the code

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 import { Media, Book, Movie, CD } from "./resources.js";
 
-// TODO
+const book = new Book('Tohir Malik', 'Alvido, bolalik', 400);
+console.log(book);
+book.addRating(9);
+book.addRating(10);
+book.addRating(8);
+console.log(book.getAverageRating());
+book.toggleCheckOutStatus();
+console.log(book);
+
+
+const movie = new Movie('Shuhrat Abbosov', 'Sen yetim emassan', 2);
+console.log(movie);
+movie.addRating(10);
+movie.addRating(9);
+movie.addRating(10);
+console.log(movie.getAverageRating());
+console.log(movie.runTime);
+console.log(movie);
+
+
+const cd = new CD('Ozodbek Nazarbekov',
+                  'Nega chaman istamas yantoq',
+                 ['Mendirman', 'Onam bo\'lsaydi']);
+
+console.log(cd);
+console.log(cd.title);
+console.log(cd.songs);
--- resources.js
+++ resources.js
@@ -1,15 +1,86 @@
 export class Media {
-  // TODO
+  constructor(title){
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+  
+  get title(){
+    return this._title;
+  }
+  
+  get isCheckedOut(){
+    return this._isCheckedOut;
+  }
+  
+  get ratings(){
+    return this._ratings;
+  }
+
+  getAverageRating(){
+    const curRatings = this._ratings;
+    const curRatingsLength = curRatings.length;
+    const curRatingsSum = curRatings.reduce((acc, curValue) => acc + curValue);
+    const avrRating = curRatingsSum / curRatingsLength;
+    
+    return avrRating;
+  }
+
+  toggleCheckOutStatus(){
+    this._isCheckedOut = !this._isCheckedOut;
+    return this._isCheckedOut;
+  }
+  
+  addRating(rating){
+    this._ratings.push(rating);
+  }
+
 }
 
-export class Book {
-  // TODO
+export class Book extends Media {
+  constructor(author, title, pages){
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+  
+  get author(){
+    return this._author;
+  }
+  
+  get pages(){
+    return this._pages;
+  }
 }
 
-export class Movie {
-  // TODO
+export class Movie extends Media {
+  constructor(director, title, runTime){
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+  
+  get director(){
+    return this._director;
+  }
+  
+  get runTime(){
+    return this._runTime;
+  }
 }
 
-export class CD {
-  // TODO
+export class CD extends Media {
+  constructor(artist, title, songs){
+    super(title);
+    this._artist = artist;
+    this._songs = songs;
+  }
+
+  get artist(){
+    return this._artist;
+  }
+  
+  get songs(){
+    return this._songs;
+  }
 }
