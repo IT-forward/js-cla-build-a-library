ee998d7f6af21bf3e28a66ebe36e88b15d669be3
Eldorbek7147 <eldor7147@gmail.com>
2021-01-04 15:17:13 +0500

    eldor's solution

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import { Media, Book, Movie, CD } from "./resources.js";
+let bookName = new Book("Marijin Haverbeke","Eloquent Javascript",430);
+bookName.addRating(1);
+console.log(bookName);
 
-// TODO
+let movieName = new Movie("Alijonov Valijov","Tarmoqdagi tuzoq", 135);
+movieName.toggleCheckOutStatus();
+movieName.addRating(5);
+console.log(movieName);
+
+let musicName = new CD("Shoxrux","Shukrona",['Rep']);
+musicName.toggleCheckOutStatus();
+musicName.addRating(2);
+musicName.addRating(8);
+console.log(musicName);
+console.log(musicName.getAverageRating());
